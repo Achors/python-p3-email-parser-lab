a068190c474a9a17776f09eae522c2926c8c2c98
Adolf <adolfnyerere@gmail.com>
2023-12-18 03:06:26 +0300

    asserted

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,15 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+
+        addresses_regex = re.findall(r'\S+@\S+', self.email_addresses)        
+        
+        addresses_split = [address.strip() for address in re.split(r'[,\s]+', self.email_addresses) if address and '@' in address]
+        
+        unique_addresses = sorted(set(addresses_regex + addresses_split))
+
+        return unique_addresses
